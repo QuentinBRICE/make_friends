340c07f285b482a9acf7c2e0e8b24d37369720e9
linda abrouk <abrouk.linda91@hotmail.com>
2023-11-21 11:19:37 +0100

    routes and pages-controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  root to: 'users#index'
+  resources :advertisements, except: [:edit, :update] do
+    resources :bookings, only: [:new, :create]
+  end
   # root "articles#index"
 end
