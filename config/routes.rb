703a055a6e3ebbacbe86af9196450250bd5b6a56
linda abrouk <abrouk.linda91@hotmail.com>
2023-11-21 13:01:58 +0100

    View & routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root to: 'users#index'
-  resources :advertisements, except: [:edit, :update] do
+  root to: 'pages#home'
+  resources :advertisements, except: [:show, :edit, :update] do
     resources :bookings, only: [:new, :create]
   end
   # root "articles#index"
