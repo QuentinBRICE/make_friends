fade1ae89babe83f05b82f32ac5ddc1ee8ee9433
linda abrouk <abrouk.linda91@hotmail.com>
2023-11-21 11:34:08 +0100

    pages-index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,29 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :phone_number, :age)
+  end
+
 end
