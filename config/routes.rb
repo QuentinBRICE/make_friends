4ade98672c1b01e9fee19509f4fddc359c35da52
Quentin Brice <quentin.brice.qb19@gmail.com>
2023-11-21 15:25:43 +0100

    routes-index-controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root to: 'pages#home'
-  resources :advertisements, except: [:show, :edit, :update] do
+  resources :advertisements, only: [:index, :new, :create,:show, :edit, :update] do
     resources :bookings, only: [:new, :create]
   end
+    resources :bookings, only: [:destroy]
   # root "articles#index"
 end
