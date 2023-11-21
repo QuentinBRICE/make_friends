d1f10b3e6d99c69885a170ea227301d03d708b50
linda abrouk <abrouk.linda91@hotmail.com>
2023-11-21 11:47:24 +0100

    view pages home & route

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
+  def home
+  end
+
   def index
     @advertisements = Advertisement.all
   end
@@ -20,9 +23,6 @@ class PagesController < ApplicationController
   end
 
   def destroy
-  end
-
-  def home
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root to: 'users#index'
+  root to: 'pages#home'
   resources :advertisements, except: [:edit, :update] do
     resources :bookings, only: [:new, :create]
   end
