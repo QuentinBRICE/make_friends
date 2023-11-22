0d4da2add431e1a52ea59d18de983a62fb6754a1
Quentin Brice <quentin.brice.qb19@gmail.com>
2023-11-22 16:33:19 +0100

    last push

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,20 @@ User.destroy_all
 
 User.create(email: "test@test.test", password: "123456")
 
-advertisement = Advertisement.create(title: 'sortie entre amis', description: "détente avec des gens biens", price: 15, address: "15 Bd des Italiens, 75002 Paris")
+advertisement = Advertisement.new(title: 'sortie entre amis', description: "détente avec des gens biens", price: 15, address: "15 Bd des Italiens, 75002 Paris")
 advertisement.user = User.all.sample
 advertisement.photo.attach(
   io: URI.open('https://img.grouponcdn.com/seocms/41NmtLyPyXHiZ1CYBg6sBMkoEtYN/Friends-eating-together_jpg-1079x648'),
   filename: 'sortie.jpg', # use the extension of the attached file here (found at the end of the url)
   content_type: 'image/jpg' # use the mime type of the attached file here
 )
+advertisement.save!
 
-advertisement = Advertisement.create(title: 'bowling entre amis', description: "détente avec des gens biens", price: 20, address: "5 Bd des Italiens, 75002 Paris")
+advertisement = Advertisement.new(title: 'bowling entre amis', description: "détente avec des gens biens", price: 20, address: "5 Bd des Italiens, 75002 Paris")
 advertisement.user = User.all.sample
 advertisement.photo.attach(
   io: URI.open('https://as2.ftcdn.net/v2/jpg/02/10/01/69/1000_F_210016964_RnRplmGfDDzO7iZfn7nd2gcWCyaW2cQK.jpg'),
   filename: 'bowling.jpg', # use the extension of the attached file here (found at the end of the url)
   content_type: 'image/jpg' # use the mime type of the attached file here
 )
+advertisement.save!
