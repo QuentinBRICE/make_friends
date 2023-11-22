e18d0ef63ee77bbd600599309b3a659444db0814
Quentin Brice <quentin.brice.qb19@gmail.com>
2023-11-22 14:25:58 +0100

    mapbox-fini

--- db/migrate/20231122105230_remove_date_from_advertisements.rb
+++
@@ -1,5 +0,0 @@
-class RemoveDateFromAdvertisements < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :advertisements, :date, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_22_114738) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_advertisements_on_user_id"
   end
 
