c091dac7b4d37c6aadca92fbaaf21b6ec796cba2
Quentin Brice <quentin.brice.qb19@gmail.com>
2023-11-22 16:32:22 +0100

    ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,12 @@
 
 require 'open-uri'
 
+Advertisement.destroy_all
+User.destroy_all
+
 User.create(email: "test@test.test", password: "123456")
 
-advertisement = Advertisement.create(title: 'sortie entre amis', description: "détente avec des gens biens", price: 15, address: "15 Bd des Italiens, 75002 Paris", user_id: 1)
+advertisement = Advertisement.create(title: 'sortie entre amis', description: "détente avec des gens biens", price: 15, address: "15 Bd des Italiens, 75002 Paris")
 advertisement.user = User.all.sample
 advertisement.photo.attach(
   io: URI.open('https://img.grouponcdn.com/seocms/41NmtLyPyXHiZ1CYBg6sBMkoEtYN/Friends-eating-together_jpg-1079x648'),
@@ -11,7 +14,7 @@ advertisement.photo.attach(
   content_type: 'image/jpg' # use the mime type of the attached file here
 )
 
-advertisement = Advertisement.create(title: 'bowling entre amis', description: "détente avec des gens biens", price: 20, address: "5 Bd des Italiens, 75002 Paris", user_id: 1)
+advertisement = Advertisement.create(title: 'bowling entre amis', description: "détente avec des gens biens", price: 20, address: "5 Bd des Italiens, 75002 Paris")
 advertisement.user = User.all.sample
 advertisement.photo.attach(
   io: URI.open('https://as2.ftcdn.net/v2/jpg/02/10/01/69/1000_F_210016964_RnRplmGfDDzO7iZfn7nd2gcWCyaW2cQK.jpg'),
