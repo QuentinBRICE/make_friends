71daf54240605890c6acd78cb51577c44005b5e2
Quentin Brice <quentin.brice.qb19@gmail.com>
2023-11-23 13:32:21 +0100

    form-booking pas fini

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   resources :advertisements, only: [:index, :new, :create,:show, :edit, :update] do
-    resources :bookings, only: [:index, :new, :create]
+    resources :bookings, only: [:index, :new, :create, :update]
     resources :dashboards, only: [:index, :new, :show]
   end
     # resources :bookings, only: [:destroy]
