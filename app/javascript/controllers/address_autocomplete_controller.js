b09bd13c1c9f23da654c37a04d11ec43360dff39
Quentin Brice <quentin.brice.qb19@gmail.com>
2023-11-24 12:01:20 +0100

    fix

--- app/javascript/controllers/address_autocomplete_controller.js
+++ app/javascript/controllers/address_autocomplete_controller.js
@@ -7,10 +7,6 @@ export default class extends Controller {
 
   static targets = ["address"]
   connect() {
-    console.log("hello from autocomplete controller");
-    // console.log(this.addressTarget); input address
-    // console.log(this.element); div data controller
-    // console.log(this.geocoder); ?
     this.geocoder = new MapboxGeocoder({
       accessToken: this.apiKeyValue,
       types: "country,region,place,postcode,locality,neighborhood,address"
@@ -22,7 +18,6 @@ export default class extends Controller {
   }
 
   #setInputValue(event) {
-    console.log(event.result["place_name"]); // Value de l'input donc l'address
     this.addressTarget.value = event.result["place_name"]
   }
 
